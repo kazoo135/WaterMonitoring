e0881e8ddfd1e5e784c7518f043e5f3f0c68c5ce
Chris Robbins <chris@Chriss-MacBook-Pro.local>
2017-11-29 12:12:13 -0500

    initial

--- server.js
+++ server.js
@@ -10,6 +10,22 @@ app.get("/sayHello", function (request, response) {
   response.end("Hello " + user_name + "!");
 });
 
+var mydb;
+var me = '678f717f-f9aa-4988-b0fd-c5f914c84cc6-bluemix';
+var password = 'f2f0a81d7190bb8dc1d691bee2ece6ada0ff730b1c527355d49b892430fd36a6'
+var Cloudant = require('cloudant');
+var cloudant = Cloudant({account:me, password:password});
+Cloudant({url:"https://678f717f-f9aa-4988-b0fd-c5f914c84cc6-bluemix.cloudant.com", username:me, password:password}, function(er, cloudant, reply) {
+  if (er)
+    throw er
+
+  console.log('Connected with username: %s', reply.userCtx.name)
+})
+
+cloudant.db.list(function(err, allDbs) {
+  console.log('All my databases: %s', allDbs.join(', '))
+});
+
 app.listen(port);
 console.log("Listening on port ", port);
 
