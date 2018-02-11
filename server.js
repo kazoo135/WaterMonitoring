1b6294a256e1499063f3d6cad9511168ddb8539e
Mark Caruso <kazoo6723@gmail.com>
2018-02-11 09:49:44 -0500

    removed db.js file - made connection to cloudant in server.js using .env vars , encapsulating cloudant credentials

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var dotenv = require('dotenv').config();
+var Cloudant = require('cloudant');
 
 
 //set view engine
@@ -17,20 +18,37 @@ app.use(require('./routes/index'));
 
 //connect to cloudant 
 var mydb;
-var username = '678f717f-f9aa-4988-b0fd-c5f914c84cc6-bluemix';
-var password = 'f2f0a81d7190bb8dc1d691bee2ece6ada0ff730b1c527355d49b892430fd36a6'
-var Cloudant = require('cloudant');
+
+var dbUrl = process.env.DB_URL;
+var username = process.env.DB_USER;
+var password = process.env.DB_PASSWORD;
 var cloudant = Cloudant({account:username, password:password});
-Cloudant({url:"https://678f717f-f9aa-4988-b0fd-c5f914c84cc6-bluemix.cloudant.com", username:username, password:password}, function(er, cloudant, reply) {
+
+Cloudant({url:dbUrl, username:username, password:password}, function(er, cloudant, reply) {
   if (er)
     throw er
 
   console.log('Connected with username: %s', reply.userCtx.name)
 })
 
-cloudant.db.list(function(err, allDbs) {
-  console.log('All my databases: %s', allDbs.join(', '))
+//list all available dbs
+var allDbs = [];
+cloudant.db.list( function(err, body ) {
+  body.forEach(function(db){
+    allDbs.push(db);
+    console.log(db);
+  })
 });
+
+//select db from cloudant
+var envdata2 = cloudant.db.use('envdata2');
+envdata2.list(function(err, body ){
+  if(!err){
+    body.rows.forEach((doc) => {
+      console.log(doc);
+    })
+  }
+})
 
 
 app.listen( app.get('port'), function(){
